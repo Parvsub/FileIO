8725a2e169c5ffa437e3a4cec0f98072a74ea8dc
Parvsub <Praveenboda421@gmail.com>
2023-03-13 15:24:52 +0530

    Added: Save in Files

---
+++ CreateEmployeeDetailsinFiles.java
@@ -0,0 +1,32 @@
+package com.employeeserviceproblem;
+
+import java.io.*;
+
+public class CreateEmployeeDetailsinFiles implements Serializable {
+
+    int id;
+    String name;
+    double salary;
+    public CreateEmployeeDetailsinFiles(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateEmployeeDetails{" + "id=" + id + ", name='" + name + '\'' +
+                ", salary=" + salary + '}';
+    }
+}
+class EmployeeStore{
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Users\\bodap\\OneDrive\\Desktop\\Praveen");
+        CreateEmployeeDetailsinFiles employeeDetails = new CreateEmployeeDetailsinFiles(23415,"Praveen",45000);
+        System.out.println(employeeDetails);
+        ObjectOutputStream ObjStream = new ObjectOutputStream(new FileOutputStream(file));
+        ObjStream.writeObject(employeeDetails);
+        ObjStream.close();
+    }
+}
+
